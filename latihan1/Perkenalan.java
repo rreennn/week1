69529d6e3cf25aae844d050e1aa2ce379de74c1d
rreennn <116095580+rreennn@users.noreply.github.com>
2024-03-12 20:47:44 +0700

    Add files via upload

---
+++ latihan1/Perkenalan.java
@@ -0,0 +1,12 @@
+package latihan1;
+
+public class Perkenalan {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Halo, nama saya adalah Keren Karenaningtyas Widi");
+		System.out.println("Saya adalah mahasiswi Teknik Informatika Universitas Dian Nuswantoro");
+		System.out.println("Saya sedang menulis program Java");
+	}
+
+}
